36a8179f29a0f26b2ca1acf7aca992d3efee0f77
fobewp <83747675+fobewp@users.noreply.github.com>
2023-02-20 21:19:45 +0100

    Added two new queries

--- pendingstat.py
+++ pendingstat.py
@@ -15,21 +15,26 @@ def update_stat(data):
 	m = re.search('(?s)(.*)( \|-\n \|\})', text)
 	insert = ' |-\n | {{subst:LOCALYEAR}}-{{subst:LOCALMONTH}}-{{subst:LOCALDAY2}}\n'
 	for i in data:
-		insert += ' | {{szám|'+str(i)+'}}\n'
+		if '%' in str(i):
+			insert += ' | '+str(i)+'\n'
+		else:
+			insert += ' | {{szám|'+str(i)+'}}\n'
 	print(insert)
 	page.edit(m.group(1) + insert + m.group(2), 'Bot: Táblázat  frissítése')
 
 
 conn = toolforge.connect('huwiki','analytics')
 queries = [
+	"SELECT ROUND(AVG(DATEDIFF(CURDATE(), CAST(CONCAT(FLOOR(fpp_pending_since/10000000000),'-',LPAD(FLOOR(fpp_pending_since/100000000) MOD 100,2,'0'),'-',LPAD(FLOOR(fpp_pending_since/1000000) MOD 100,2,'0')) AS date))),0) FROM flaggedpage_pending INNER JOIN page on page_id = fpp_page_id",
+	"SELECT CONCAT(REPLACE(ROUND(100*(((SELECT COUNT(*) FROM flaggedpages INNER JOIN page ON page_id = fp_page_id WHERE page_namespace = 0)-(SELECT COUNT(*) FROM flaggedpage_pending INNER JOIN page ON page_id = fpp_page_id WHERE page_namespace = 0))/(SELECT COUNT(*) FROM page WHERE page_namespace = 0)),2),'.',','),'%')",
 	"SELECT COUNT(rev_id) FROM revision INNER JOIN page ON rev_page = page_id INNER JOIN flaggedpage_pending ON page_id = fpp_page_id WHERE rev_id > fpp_rev_id;",
 	"SELECT COUNT(DISTINCT rev_id) FROM revision INNER JOIN page ON rev_page = page_id INNER JOIN flaggedpage_pending ON page_id = fpp_page_id INNER JOIN actor ON actor_id = rev_actor LEFT OUTER JOIN user_groups ON ug_user = actor_user WHERE rev_id > fpp_rev_id AND ug_group IN ('editor','trusted','sysop','bot');",
 	"SELECT COUNT(*) FROM flaggedpage_pending",
 	"SELECT COUNT(DISTINCT page_id) FROM revision INNER JOIN page ON rev_page = page_id INNER JOIN flaggedpage_pending ON page_id = fpp_page_id INNER JOIN actor ON actor_id = rev_actor LEFT OUTER JOIN user_groups ON ug_user = actor_user WHERE rev_id > fpp_rev_id AND ug_group IN ('editor','trusted','sysop','bot');"
 ]
-num = [None] * 4;
+num = [None] * len(queries);
 with conn.cursor() as cur:
-	for i in range(4):
+	for i in range(len(queries)):
 		print("Query "+str(i)+"...", end="")
 		rows = cur.execute(queries[i])
 		print(" executed", end="")
